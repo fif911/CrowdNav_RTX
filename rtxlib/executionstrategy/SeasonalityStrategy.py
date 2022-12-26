2c2acd4f81d61fe02f9a6956c1d650fca76e1fff
Alex Zakotyanskyi <o.zakotianskyi@student.vu.nl>
2022-12-26 16:49:43 +0100

    feat: connect kafka to RTX with tick; connect Traffic generator

--- rtxlib/executionstrategy/SeasonalityStrategy.py
+++ rtxlib/executionstrategy/SeasonalityStrategy.py
@@ -11,6 +11,7 @@ from colorama import Fore
 
 from rtxlib import info, error
 from rtxlib.execution import experimentFunction
+from rtxlib.trafficprovider.TrafficParser import TrafficGenerator
 
 
 def start_seasonality_strategy(wf):
@@ -20,15 +21,34 @@ def start_seasonality_strategy(wf):
     info(f"Total experiments: {wf.totalExperiments}")
 
     # knob gener
+    # tg = TrafficGenerator(0)
+    tg = TrafficGenerator(200)
+    while True:
+        # read kafka
+        wf.secondary_data_providers[0]['instance'].reset()
+        new_data = wf.secondary_data_providers[0]["instance"].returnData()
+        print(new_data)
+        c_tick = new_data['tick']
+        traffic_volume = tg(c_tick)
 
+        msg = {
+            "total_car_counter": traffic_volume,
+            "car_counter_is_initial": False
+        }
 
-    for exp_num, kn in enumerate(wf.execution_strategy["knobs"]):
-        info(f"Total experiments: {wf.totalExperiments}")
-        info(f"Running experiment: {exp_num + 1}/{wf.totalExperiments}")
-        print("Setting for current experiment:")
-        pprint(kn)
-        experimentFunction(wf, {
-            "knobs": kn,
-            "ignore_first_n_results": wf.execution_strategy["ignore_first_n_results"],
-            "sample_size": wf.execution_strategy["sample_size"],
-        })
+        # apply changes to system
+        try:
+            wf.change_provider["instance"].applyChange(msg)
+        except:
+            error("apply changes did not work")
+
+        # for exp_num, kn in enumerate(wf.execution_strategy["knobs"]):
+        #     info(f"Total experiments: {wf.totalExperiments}")
+        #     info(f"Running experiment: {exp_num + 1}/{wf.totalExperiments}")
+        #     print("Setting for current experiment:")
+        #     pprint(kn)
+        # res = experimentFunction(wf, {
+        #     "knobs": kn,
+        #     "ignore_first_n_results": wf.execution_strategy["ignore_first_n_results"],
+        #     "sample_size": wf.execution_strategy["sample_size"],
+        # })
